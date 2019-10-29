141986bbe1bc64e4f3282cc67659c1fbe1cd5c9b
jzquinzon <jzquinzon@gmail.com>
2019-10-28 20:29:18 -0700

    updated

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -3,23 +3,26 @@ from BoardClasses import Move
 from BoardClasses import Board
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
+
+
 class StudentAI():
 
-    def __init__(self,col,row,p):
+    def __init__(self, col, row, p):
         self.col = col
         self.row = row
         self.p = p
-        self.board = Board(col,row,p)
+        self.board = Board(col, row, p)
         self.board.initialize_game()
         self.color = ''
-        self.opponent = {1:2,2:1}
+        self.opponent = {1: 2, 2: 1}
         self.color = 2
-    def get_move(self,move):
+
+    def get_move(self, move):
         if len(move) != 0:
-            self.board.make_move(move,self.opponent[self.color])
+            self.board.make_move(move, self.opponent[self.color])
         else:
             self.color = 1
-        moves = self.board.get_all_possible_moves(self.color) # list of moves
+        moves = self.board.get_all_possible_moves(self.color)  # list of moves
 
         #best_move = moves[0][0]
         # for i in moves:
@@ -33,53 +36,54 @@ class StudentAI():
         #   self.undo()
         # move= bestmove
 
-       
         #Initializing the best_move/score as the first possible move/score
         best_move = moves[0][0]
         self.board.make_move(best_move, self.color)
-        best_score = self.board_score() 
-        self.board.undo()               
-
+        best_score = self.board_score()
+        self.board.undo()
 
         for i in moves:
+            for ii in i:
 
-            #changing to new gamestate
-            self.board.make_move(i, self.color)
+                #changing to new gamestate
+                self.board.make_move(ii, self.color)
 
-            #grabbing list of opponents moves
-            opponents_moves = self.board.get_all_possible_moves(self.opponent[self.color])
+                #grabbing list of opponents moves
+                opponents_moves = self.board.get_all_possible_moves(
+                    self.opponent[self.color])
 
-            #initializing the min_move/scre as the first possible move of the opponent
-            min_move = opponents_moves[0][0];
-            self.board.make_move(min_move, self.opponent[self.color])    
-            min_score = self.board_score()   
-            self.board.undo()
-
-            #itterate through opponents moves to find the min value
-            for j in opponents_moves:        
-                self.board.make_move(j)
-
-                # Find the min between min_move and j
-                if(min_score > self.board_score):
-                    min_score = self.board_score
-                    min_move = j
-
-                # set min_move to the new min
+                #initializing the min_move/scre as the first possible move of the opponent
+                min_move = opponents_moves[0][0]
+                self.board.make_move(min_move, self.opponent[self.color])
+                min_score = self.board_score()
                 self.board.undo()
-            
-            # Set the best_move to max of 
-            # min_move and best_move
-            if(best_score < min_score):
-                best_score = min_score
-                best_move = i
 
-            self.board.undo()
+                #itterate through opponents moves to find the min value
+                for j in opponents_moves:
+                    for jj in j:
+                        self.board.make_move(jj,self.opponent[self.color])
+
+                        # Find the min between min_move and j
+                        if( min_score > self.board_score() ):
+                            min_score = self.board_score
+                            min_move = jj
+
+                        # set min_move to the new min
+                        self.board.undo()
+                # Set the best_move to max of
+                # min_move and best_move
+                if(best_score < min_score):
+                    best_score = min_score
+                    best_move = ii
+
+                self.board.undo()
         move = best_move
-        self.board.make_move(move,self.color)
+        self.board.make_move(move, self.color)
         return move
 
     def board_score(self):
-        w_points = 0; b_points = 0;
+        w_points = 0
+        b_points = 0
         for c in range(self.col):
             for r in range(self.row):
                 if self.board.board[c][r].get_color() == 'W':
@@ -91,7 +95,7 @@ class StudentAI():
                     if self.board.board[c][r].is_king == True:
                         b_points += 1.2
                     else:
-                        b_points += 1;
+                        b_points += 1
                 else:
                     pass
-        return w_points - b_points;
+        return w_points - b_points
