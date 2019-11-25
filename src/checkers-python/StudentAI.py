3cc8d7b413f19c87af0102ba70c79414d854a47d
jzquinzon <jzquinzon@gmail.com>
2019-11-24 20:17:11 -0800

    updated msg

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -121,7 +121,7 @@ class StudentAI():
 
                 if current_piece.get_color() == self.color:
                     if current_piece.is_king == True:
-                        player_points += 4000
+                        player_points += 100000
                     else:
                         player_points += 1000
                         if self.color == 2:
@@ -130,7 +130,7 @@ class StudentAI():
                             player_points += (r / self.row) * 1000
                 else:
                     if current_piece.is_king == True:
-                        opponent_points += 4000
+                        opponent_points += 100000
                     else:
                         opponent_points += 1000
                         if self.opponent[self.color] == 2:
